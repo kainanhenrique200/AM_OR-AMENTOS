65e6de19ee132f431775e56824eb0fec3bd8f91f
kainanhenrique200 <kainanhenrique200@gmail.com>
2026-07-02 13:40:20 -0300

    Corrige models/material.py para usar Produto e herança correta

--- models/material.py
+++ models/material.py
@@ -1,7 +1,13 @@
-class Material:
-    def __init__(self, nome, preco_m2):
+# models/material.py
+class Produto:
+    def __init__(self, nome):
         self.nome = nome
+
+
+class Material(Produto):
+    def __init__(self, nome, preco_m2):
+        super().__init__(nome)
         self.preco_m2 = preco_m2
 
     def __str__(self):
-        return f"Material: {self.nome} | Preço por m2: R$ {self.preco_m2:.2f}"
+        return f"Material: {self.nome} | Preço por m²: R$ {self.preco_m2:.2f}"
